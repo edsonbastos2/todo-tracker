73deab7245af4052336118394796fa06564728cd
Edson Bastos <abastos@uolin.com>
2022-10-28 18:33:09 -0300

    feat: adicionado props e evento

---
+++ src/components/Buttom.vue
@@ -0,0 +1,40 @@
+<template>
+    <button class="button" @click="handleClick" :disabled="disabled">
+      <span class="icon">
+        <i :class="`fas fa-${icon}`"></i>
+      </span>
+      <span>{{text}}</span>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'ButtomTime',
+    emits:['click'],
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        icon: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    methods:{
+        handleClick() {
+            this.$emit('click')
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
